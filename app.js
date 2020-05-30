ebc5dc42c393e6100126a726509cfaad21d24589
Firman <fblazt@outlook.com>
2020-05-30 14:16:11 +0700

    Try get static data

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const app = express()
+
+app.use('/api/stuff', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'My first thing',
+      description: 'All of the info about my first thing',
+      imageUrl: '',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'My second thing',
+      description: 'All of the info about my second thing',
+      imageUrl: '',
+      price: 2900,
+      userId: 'qsomihvqios',
+    }
+  ]
+  res
+    .status(200)
+    .json(stuff)
+})
+
+module.exports = app
