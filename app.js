0fabcb22d815aad35dbd26ef7292b9652c1aa046
Firman <fblazt@outlook.com>
2020-05-30 18:42:54 +0700

    Connect API to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-snaef.mongodb.net/test?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!')
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!')
+    console.error(error)
+  })
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
