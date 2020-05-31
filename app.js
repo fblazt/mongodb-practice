a7f7fda4d6ac39374a4ede5d88636c84f80ddb09
Firman <fblazt@outlook.com>
2020-05-31 14:58:48 +0700

    Add update and delete request

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post('/api/stuff', (req, res, next) => {
     })
 })
 
-app.use('/api/stuff/:id', (req, res, next) => {
+app.get('/api/stuff/:id', (req, res, next) => {
   Thing.findOne({
     _id: req.params.id
   })
@@ -66,7 +66,51 @@ app.use('/api/stuff/:id', (req, res, next) => {
     })
 })
 
-app.use('/api/stuff', (req, res, next) => {
+app.put('/api/stuff/:id', (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId 
+  })
+  Thing.updateOne({_id: req.params.id}, thing)
+    .then(() => {
+      res
+        .status(201)
+        .json({
+          message: 'Thing updated successfully!'
+        })
+    })
+    .catch((error) => {
+      res
+        .status(400)
+        .json({
+          error: error
+        })
+    })
+})
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({_id: req.params.id})
+    .then(() => {
+      res
+        .status(200)
+        .json({
+          message: 'Thing deleted successfully!'
+        })
+    })
+    .catch((error) => {
+      res
+        .status(400)
+        .json({
+          error: error
+        })
+    })
+})
+
+app.get('/api/stuff', (req, res, next) => {
   Thing.find()
     .then((things) => {
       res
