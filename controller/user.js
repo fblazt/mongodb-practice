ff08f7e5f2b8f98c931a1816732e3f85f127dfd8
Firman <fblazt@outlook.com>
2020-06-05 15:24:27 +0700

    Create authentication tokens

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
 exports.signup = (req, res, next) => {
@@ -41,11 +43,15 @@ exports.login = (req, res, next) => {
               error: new Error('Incorrect password!')
             })
           }
+          const token = jwt.sign(
+            {userId: user._id},
+            `${process.env.AUTH_TOKEN}`,
+            {expiresIn: '24h'})
           res
             .status(200)
             .json({
               userId: user._id,
-              token: 'token'
+              token: token
             })
         })
         .catch((error) => {
